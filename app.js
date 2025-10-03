8cce94629c0ed9f63f7306cc3f4f5bd9d2ef08cf
avijit <avijitjana@gmail.com>
2025-10-03 10:12:04 +0530

    login api aaded

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dbConnection = require("./config/dbConfiguration");
 const User = require("./models/users.model.js");
+const bcrypt = require("bcrypt");
 
 //initilize express into app
 const app = express();
@@ -9,20 +10,54 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 7000;
 
-
+//signup
 app.post("/signup", async (req, res) => {
-    const user = new User(req.body);
     try {
+
+        const {firstName, lastName, emailId, password, about} = req.body;
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: hashPassword, 
+            about
+        });
+
         await user.save();
 
         res.send("User added successfully");
 
     } catch (err) {
-        res.status(400).send("error in saving user details on signup", err.message);
+        res.status(400).send("error in saving user details on signup" + err.message);
     }
 
 });
 
+//login
+app.post("/login", async(req,res)=>{
+    try{
+        const {emailId, password} = req.body;
+
+        const result = await User.findOne({emailId: emailId});
+        if(result){
+            const isPassword  = await bcrypt.compare(password, result.password);
+            if(isPassword){
+                res.status(200).send("Login successfull");
+            }else{
+                res.status(400).send("invalid credentials");
+            }
+        }else {
+            res.status(404).send("data not found in the database");
+        }
+
+    }catch(err){
+        res.status(500).send("error in login. reason is" +  err.message);
+    }
+})
+
+//get user details by emailId
 app.get("/user", async (req, res) => {
     const user = req?.body?.emailId;
     try {
@@ -35,9 +70,27 @@ app.get("/user", async (req, res) => {
         res.send("successfully fetched user details by emailId", result[0]);
 
     } catch (err) {
-        res.status(400).send("error in fetching user details", err.message);
+        res.status(400).send("error in fetching user details" + err.message);
     }
 
+});
+
+//update user data by emailId
+app.patch("/user/update", async (req, res) => {
+    const userEmailId = req.body.emailId;
+    const userFirstName = req.body.firstName;
+
+    try {
+        if (userEmailId) {
+            const result = User.updateOne({ emailId: userEmailId }, { firstName: userFirstName });
+            if (!result) res.status(404).send("could not update user details" + result.err);
+            res.status(200).send("user data updated successfully");
+        } else {
+            res.status(400).send("emailId should be required to update user data");
+        }
+    } catch (err) {
+        res.status(500).send("internal server error" + err.message);
+    }
 });
 
 dbConnection().then(() => {
--- models/users.model.js
+++ models/users.model.js
@@ -1,26 +1,63 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        masLenght: 50
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Please enter proper email address");
+            }
+        }
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error("Please enter strong password");
+            }
+        }
     },
     age: {
         type: Number
     },
     gender: {
-        type: String
-    }
-})
+        type: String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Please put valid gender!");
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://picsum.photos/id/237/200/300",
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Please pass proper image url");
+            }
+        }
 
-const User = mongoose.model("User", UserSchema);  
+    },
+    about: {
+        type: Array(String)
+    }
+});
+
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
