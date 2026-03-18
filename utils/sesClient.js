746c4f1d49e07b77ed4d4132cd09e98fb7ef16cd
avijit <avijitjana@gmail.com>
2026-03-18 09:33:23 +0530

    build send email system using AWS SES

---
+++ utils/sesClient.js
@@ -0,0 +1,11 @@
+const { SESClient } = require("@aws-sdk/client-ses");
+// Set the AWS Region.
+const REGION = "ap-south-1";
+// Create SES service object.
+const sesClient = new SESClient({
+    region: REGION, credentials: {
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY
+    }
+});
+module.exports = { sesClient };
