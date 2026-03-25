5c665392b3410684d14e6ebfb7d96dc3d5e547b0
avijit <avijitjana@gmail.com>
2026-03-25 13:00:30 +0530

    dummy push

--- common/constants.js
+++ common/constants.js
@@ -1,6 +1,6 @@
 const BAD_REQUEST = "Bad request";
 const INVALID_DATA_STRING = "It seems that you have entered invalid data! Please check the data you have entered"
-const UNKNOWN_ERROR_MESSAGE = "Something went wrong";
+const UNKNOWN_ERROR_MESSAGE = "Something went wrong"; 
 
 module.exports = {
     BAD_REQUEST,
